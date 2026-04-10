9a88efb0c18b0d8877044174c13bb37daeb3a5be
안성주 <124550549+PastelJ321@users.noreply.github.com>
2026-04-10 12:05:42 +0900

    Add files via upload

---
+++ members.js
@@ -0,0 +1,17 @@
+window.memberPages = [
+  // 예시
+  // {
+  //   name: "김민아",
+  //   description: "감성적인 자기소개와 좋아하는 것들을 담은 페이지",
+  //   href: "mina.html"
+  // },
+  // {
+  //   name: "이지호",
+  //   description: "프로필, 취미, 목표를 정리한 개인 홈페이지",
+  //   href: "jiho.html"
+  // }
+  {
+    name: "송범준",
+    href: "송범준.html"
+  }
+];
