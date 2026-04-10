4e57e7423504d522cb5b846c37fb4697a36d26b1
안성주 <124550549+PastelJ321@users.noreply.github.com>
2026-04-10 12:24:28 +0900

    Add files via upload

--- members.js
+++ members.js
@@ -1,32 +1,38 @@
 window.memberPages = [
   {
+    name: "곽호윤",
+    href: "곽호윤.html"
+  },
+  {
     name: "박수현",
-    description: "파이 테마로 만든 개인 소개 페이지",
     href: "10609박수현/index.html"
   },
   {
     name: "김은서",
-    description: "자기소개와 미니 게임이 있는 페이지",
     href: "20308김은서/index.html"
   },
   {
     name: "김시우",
-    description: "취미를 소개하는 카드형 페이지",
     href: "김시우/Untitled-1.html"
   },
   {
     name: "박시후",
-    description: "자기소개와 관심사를 정리한 페이지",
     href: "박시후/index.html"
   },
   {
     name: "송범준",
-    description: "쇼핑몰 스타일로 만든 미니 홈페이지",
-    href: "송범준/송범준.html"
+    href: "송범준.html"
   },
   {
     name: "이준우",
-    description: "오렌지 톤의 랜딩 페이지 스타일 홈페이지",
-    href: "이준우/이준우.HTML"
+    href: "이준우.HTML"
+  },
+  {
+    name: "이현준",
+    href: "이현준/index.html"
+  },
+  {
+    name: "정지훈",
+    href: "정지훈홈페이지.html"
   }
 ];
